0f61f326bcd04439478fac39ea285cf88cdbddc2
pedro-surf <40304742+pedro-surf@users.noreply.github.com>
2019-04-04 12:08:41 -0300

    Added component with issue
    https://stackoverflow.com/questions/55518690/unable-to-perform-get-request-using-state-as-query-parameter

---
+++ App.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+
+
+
+class App extends Component {
+  
+    state = {
+        loading: false,
+        query_brand: 0,
+        query_model: "",
+        brands: [],
+        models: [],
+        years: []
+      
+    
+    };
+ 
+
+    //Functions to handle state when some query is sent
+    // Get all brands first
+    getBrands = () => axios.get('https://parallelum.com.br/fipe/api/v1/carros/marcas')
+                        .then(res => this.setState({ brands: res.data }))
+                        .catch(err => console.log(err));
+
+        
+   getModels = async () => {
+       if (this.state.query_brand !== 0) {
+            this.setState({ loading: "Fetching models..." });
+            let brand = this.state.query_brand;
+           await axios.get(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${brand}/modelos/`)
+                    .then(res => this.setState({ models: res.data }))
+                    .catch(err => console.log(err));
+        }};
+  onChangeHandler = async (e) => {
+    this.setState({ [e.target.name]: e.target.value, target: e.target.name });
+      this.getModels();
+  };
+   
+
+ componentDidMount() {
+     this.getBrands();
+ }
+
+
+  render() {
+        const selectBrandOptions = this.state.brands.map(brand => (
+    <option key={brand.codigo} value={brand.codigo}>
+      {brand.nome}
+    </option>
+            ));
+     const selectModelOptions = this.state.models.map(model => (
+     <option key={model.codigo} value={model.codigo}>
+          {model.nome}
+    </option>    
+            ));    
+      
+            
+      
+    return (
+    <div className="App">
+        <div className="col-3 p-4 d-inline-block">
+        
+            <select 
+            value={this.state.query_brand} 
+            onChange={this.onChangeHandler} 
+            name="query_brand" 
+            className="form-control"
+             >
+        {selectBrandOptions}
+            </select>
+        Selecione a marca:
+        </div>
+        <div className="col-3 p-4 d-inline-block">
+            <select 
+            value={this.state.query_model} 
+            onChange={this.onChangeHandler}
+            name="query_model"
+            disabled={this.state.query_brand === 0}
+            className="form-control"
+            >
+            <option value="placeholder">blablabla</option>
+            <option value="hardcoption">blablabla</option>
+            {selectModelOptions}
+            </select>
+            Selecione o modelo:
+        </div>
+          
+        <div className="col-4 d-inline-block">
+         //TODO
+            <select key={this.state.brands} 
+                value={this.state.query_year} 
+                onChange={this.onChangeHandler}
+                name="query_year"
+                disabled={this.state.query_model === ""}
+                className="form-control"
+                >
+             <option value="placeholder option">blablabla</option>
+             <option value="placeholder option">blablabla</option>
+            </select>
+        </div>
+    </div>
+    );
+  }
+}
+
+export default App;
