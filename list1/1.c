001f9c3fb9026e9f5d049fda79a521d9031d848f
pedro-surf <40304742+pedro-surf@users.noreply.github.com>
2019-04-16 18:10:34 -0300

    updated 1.c

--- list1/1.c
+++ list1/1.c
@@ -2,7 +2,7 @@
 // EXERCISES LIST: first
 // EXERCISE NUM: 1
 // AUTHOR: Pedro Silveira
-//       
+//
 // FOR COURSE: Programming I
 // DUE DATE:    April, 2019
 //
@@ -12,11 +12,11 @@
 // 	Modifique programa permitindo:
 // 	receber a quantidade de votos a ser analisada
 // 	receber os votos para 7 candidatos
-// 	calcular a percentagem de votos de cada candidato e votos nulos 
+// 	calcular a percentagem de votos de cada candidato e votos nulos
 
 //
 // OVERALL METHOD:
-// 1. Receive votes from {{el}} electors 
+// 1. Receive votes from {{el}} electors
 // 2. Calculate votes for each of all {{m}} candidates
 // 3. Return count of approved and disapproved students as well as approved's average.
 //
@@ -32,32 +32,38 @@
 int main()
 {
 	int el;
-	int m = 5;
 	int i;
 	int votedcandidate = 0;
-	
+	// Inicializando o vetor c onde cada candidato ocupa uma posiçao
+	int c[4] = {0};
 
-	 printf("Digite o número de eleitores e, se desejar, o número de candidatos (somente 5 ou 7), separados por um espaço simples");
-	 scanf("%i %i", &el &m);
+
+	 printf("Digite o número de eleitores");
+	 scanf("%i", &el);
 
 // Verif se o nro de candidatos é um dos desejados
-	 while (m !== 5 && m !== 7) {
-	   printf("O número de candidatos deve ser 5 ou 7. Digite novamente");
-	   scanf("%i, &m);
-	  }
+	// while (m ==! 5 && m ==! 7) {
+	 //  printf("O número de candidatos deve ser 5 ou 7. Digite novamente");
+	  // scanf("%i", &m);
+	 // }
 
-// Inicializando o vetor c onde cada candidato ocupa uma posiçao
-	int c[m];
+
 // Conta os votos para cada candidato
 	for (i = 1; i <= el; i++) {
-	   printf("Selecione o numero do candidato de 1 até %i para o eleitor nro: %i ", &m &el);
+	   printf("Selecione o numero do candidato  para o eleitor nro: %i ", i);
 	   scanf("%i", &votedcandidate);
-	   while (votedcandidate > m || votedcandidate < 1) {
+	   while (votedcandidate > 5 || votedcandidate < 1) {
 	        printf("Candidato inválido");
-		printf("Selecione o numero do candidato de 1 até %i para o eleitor nro: %i ", &m &el);
+		printf("Selecione o numero do candidato de 1 até 5 para o eleitor nro: %i ", i);
 	        scanf("%i", &votedcandidate);
 	   }
+        votedcandidate = votedcandidate - 1;
 	   c[votedcandidate]++;
+
  	}
+ 	for (i = 0; i <= 5; i++ ) {
+ 	 printf("\n %i", c[i]);
+ 	 }
 
     }
+
