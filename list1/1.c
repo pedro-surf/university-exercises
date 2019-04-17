89faf631c95275320a35425f18ddc04028a29350
pedro-surf <pedroaki.55@gmail.com>
2019-04-16 21:15:38 -0300

    Improved readability

--- list1/1.c
+++ list1/1.c
@@ -1,13 +1,10 @@
-// FILE: prog_1_res_15.c
-// EXERCISES LIST: first
-// EXERCISE NUM: 1
+// FILE: 1.c
+// LIST:  first
+// EXERCISE NUMBER: 1
 // AUTHOR: Pedro Silveira
-//
 // FOR COURSE: Programming I
 // DUE DATE:    April, 2019
-//
 // PURPOSE:
-//       The list of general tasks is:
 //      Programa recebe as escolhas dos 10 eleitores e calcula os votos para 5 candidatos.
 // 	Modifique programa permitindo:
 // 	receber a quantidade de votos a ser analisada
@@ -17,10 +14,10 @@
 //
 // OVERALL METHOD:
 // 1. Receive votes from {{el}} electors
-// 2. Calculate votes for each of all {{m}} candidates
-// 3. Return count of approved and disapproved students as well as approved's average.
+// 2. Add votes for matching selected candidate
+// 3. Return count of votes for each candidate
 //
-// FUNCTIONS: in algorithm scope
+// FUNCTIONS: inside main func
 //
 //  INCLUDED FILES:
 //  stdio.h
@@ -28,12 +25,9 @@
 // DATA FILES: none
 #include <stdio.h>
 
-
 int main()
 {
-	int el;
-	int i, j;
-	int votedcandidate;
+	int i, el, votedcandidate;
 	int votedindex, arrayindex;
 // Inicializando o vetor c onde cada candidato ocupa uma posiçao para até 7 candidatos
 	int c[7] = { 0 };
@@ -41,25 +35,23 @@ int main()
 	 scanf("%i", &el);
 // Conta os votos para cada candidato
 	for (i = 1; i <= el; i++) {
-	   printf("Selecione o numero do candidato  para o eleitor nro: %i ", i);
+	   printf("eleitor %i : Selecione o numero do seu candidato :: ", i);
 	   scanf("%i", &votedcandidate);
 	   while (votedcandidate > 7 || votedcandidate < 1) {
 	        printf("Candidato inválido \n");
-		  printf("Selecione o numero do candidato de 1 até 5 para o eleitor nro: %i ", i);
+		  printf(" eleitor %i : Selecione o numero do seu candidato :: ", i);
 	        scanf("%i", &votedcandidate);
 	   }
 	   votedindex = votedcandidate - 1;
 	   c[votedindex]++;
 	   votedcandidate = 0;
-	   
-
  	}
- 	for ( j = 1; j < 8; j++ ) {
-		arrayindex = j-1;
- 	 printf("\n Candidato %i :", j);
- 	 int indexcount = c[arrayindex];
- 	 printf("Votos: %i \n", indexcount);
-		
+ 	for ( i = 1; i < 8; i++ ) {
+		arrayindex = i-1;
+		if (c[arrayindex] > 0) {
+ 	 printf("\n Candidato %i :", i);
+ 	 printf("Votos: %i \n", c[arrayindex]);	
+		}
  	 }
 	return 0;
     }
