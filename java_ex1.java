c9f4316508c7a35e5be200a3942bfb74fdb4277d
pedro-surf <40304742+pedro-surf@users.noreply.github.com>
2019-08-08 21:53:14 -0300

    ...

--- java_ex1.java
+++ java_ex1.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class Exercises {
 int main(String [], args) {
 
- public static void main(String[] args) {
- 	System.out.println("Digite o valor de X e depois o de Y");
-	 Scanner x = new Scanner (System.in);
- 	Scanner y = new Scanner (System.in);
-        System.out.println("Soma: "+ (x + y));
-	int maior = (x >= y ? x : y);
-	System.out.println("Maior: " + maior);
-    while (x < y) {
-	x++; 
-	System.out.println(x);
-	}    
-
-     }
+  public static void main(String[] args) {
+        System.out.println("Digite o valor de X e depois o de Y");
+	 	     Scanner entrada = new Scanner(System.in);
+                     int x = entrada.nextInt();
+                     int y = entrada.nextInt();
+                    System.out.println("Soma: "+ (x + y));
+                    int maior = (x >= y ? x : y);
+                    System.out.println("Maior: " + maior);
+                    int menor = (maior == x ? y : x);
+                    menor++;
+                    while (menor < maior) {
+			System.out.println(menor);
+                        menor++;
+		}    
+    }
 
 }
