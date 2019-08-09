d74b0c73ea86f3deaac3d8de4d946a9a8a59097f
pedro-surf <40304742+pedro-surf@users.noreply.github.com>
2019-08-08 21:27:52 -0300

    My first java programs

---
+++ java_ex1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercises {
+int main(String [], args) {
+
+ public static void main(String[] args) {
+ 	System.out.println("Digite o valor de X e depois o de Y");
+	 Scanner x = new Scanner (System.in);
+ 	Scanner y = new Scanner (System.in);
+        System.out.println("Soma: "+ (x + y));
+	System.out.println("Maior: " + (x >= y ? x : y));
+    }
+
+
+}
