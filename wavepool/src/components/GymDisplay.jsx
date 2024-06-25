539b7b42b9882fb3cba5fae4a6f34e7b7802b01c
pedro-surf <pedroaki.55@gmail.com>
2024-06-25 12:56:05 -0300

    add missing component

---
+++ wavepool/src/components/GymDisplay.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const EcoGymDisplay = ({ numPeople }) => {
+  return (
+    <Box>
+      <Typography variant="h6">Eco Gym Display</Typography>
+      <Typography>Number of People: {numPeople}</Typography>
+      {/* Add graphical representation here */}
+      <Box sx={{ height: '200px', background: 'lightgreen', marginTop: '10px', position: 'relative' }}>
+        {Array.from({ length: numPeople }).map((_, index) => (
+          <Box
+            key={index}
+            sx={{
+              width: '10px',
+              height: '20px',
+              background: 'green',
+              margin: '2px',
+              display: 'inline-block'
+            }}
+          />
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default EcoGymDisplay;
