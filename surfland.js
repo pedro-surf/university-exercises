ecb3c23ebfd9b7ffcdc933c5056c6e6be0d62027
Pedro Silveira <40304742+pedro-surf@users.noreply.github.com>
2024-11-27 10:59:15 -0300

    Update surfland.js

--- surfland.js
+++ surfland.js
@@ -1,6 +1,6 @@
 const seshs = ['BARRELS', 'T4', 'BEAST', 'AVANÇADO'];
 console.log(arr.filter(val => seshs.some(s => val.friendlyName.includes(s))).map(val => ({ 
-[val.friendlyName.split('SESSÃO ')[0]: val.category, ...(val.schedules || []).reduce((acc, val) => {
+[val.friendlyName.split('SESSÃO ')[1]]: val.category, ...(val.schedules || []).reduce((acc, val) => {
 acc[val.schedule] = val.available;
 return acc;
 }, {}) })));
